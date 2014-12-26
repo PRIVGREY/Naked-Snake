f82e35088bcfc4f74dea5aae497e70d2754bfc20
PRIVGREY <privgrey@gmail.com>
2014-12-26 22:37:39 +0800

    Add ROC function

--- snake.py
+++ snake.py
@@ -1,3 +1,12 @@
 
 def fuck():
     print ('fuck the world!')
+
+def roc(date):
+
+    #This function transform datetime object to R.O.C year string
+
+    year = str(int(date.strftime('%Y')) - 1911)
+    return year
+
+
